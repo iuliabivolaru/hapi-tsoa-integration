d400a32a7a6328923194bb89e68e5b070f08bbb5
Iulia Bivolaru <iulia.bivolaru24@gmail.com>
2018-08-24 18:14:06 +0200

    Do cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,4 @@
-import * as Hapi from "hapi";
-import { App } from "./app";
-import * as http from "http";
-
-// export function init(
-// ): Hapi.Server {
-//   try {
-//     const server = new Hapi.Server({
-//       host: 'localhost',
-//       port: 3000
-//     });
-  
-//     server.start();
-
-//     return server;
-//   } catch (err) {
-//     console.log("Error starting server: ", err);
-//     throw err;
-//   }
-
-// }
-
-// const server: Hapi.Server = new Hapi.Server({
-//        host: 'localhost',
-//        port: 3000
-// })
-
-// server.route({
-//     method: "GET",
-//     path: "/",
-//     handler: function(request,h) {
-
-//       return'hello world';
-//   }
-
-// });
-
-
-// server.start();
+import App from "./app";
 
 const hapiServer = App.build();
 hapiServer.start();
